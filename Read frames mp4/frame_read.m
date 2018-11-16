3fca1940fea2573e0b7d27fa88bd4875229ad1c4
Alfonso Diaz <adiazalo@users.noreply.github.com>
2018-11-15 18:23:55 -0800

    Update frame_read.m

--- Read frames mp4/frame_read.m
+++ Read frames mp4/frame_read.m
@@ -5,12 +5,9 @@ count = 1;
 v = VideoReader(vid);
 figure;
 for t=1:nFrame
-   A(:,:)=transpose(readFrame(v,[width,height],'uint8')); 	  %Reading the Y (luminance) information%
-   %tmp=fread(fid,[width/2,height/2],'uint8');		  %Reading the U (chrominance) information%	
-   %tmp=fread(fid,[width/2,height/2],'uint8');		   %Reading the V (chrominance) information%		
+   A(:,:)=transpose(readFrame(v,[width,height],'uint8')); 	  %Reading the Y (luminance) information%	
    B = A / max(A(:));
-     imshow(B);
    %imwrite(B,sprintf('c:\\foreman%03d.jpg',t),'jpg','Quality',100); %Writing 1 frame (the luminance information) to a file%
 end
 frame_Q = double(B);
- imshow(frame_Q);
+imshow(frame_Q);
