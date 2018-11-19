81dc24003bc680284558198963f1c8e634b9a1e8
Alfonso Diaz <adiazalo@protonmail.com>
2018-11-18 17:33:20 -0800

    outputVideo done

--- Block-based codec (MP4)/test.m
+++ Block-based codec (MP4)/test.m
@@ -1,8 +1,6 @@
-i=1;
 j=2;
-
 disp("frameRate")
-[frameInit,frameRate] = read_MP4Frame('dustpan.mp4',1);
+[frame1,frameRate] = read_MP4Frame('dustpan.mp4',1);
 
 disp("outputVideo")
 outputVideo = VideoWriter('dustpan_out.avi');
@@ -11,17 +9,16 @@ open(outputVideo);
 
  while j< 10
 
-frame1 = read_MP4Frame('dustpan.mp4',i);
 frame2 = read_MP4Frame('dustpan.mp4',j);
 
 disp("motion_estimation")
-[mvx,mvy] = motion_estimation(frame1, frame2, 15, 15, 30);
+[mvx,mvy] = motion_estimation(frame1, frame2, 120, 120, 120);
 disp("mc_prediction")
 [mcpr,pred] = mc_prediction(frame1,frame2,mvx,mvy);
 pred = uint8(pred);
 
 disp("image_dct_enc")
-Nbits = image_dct_enc(mcpr,'dct.bit',10);
+Nbits = image_dct_enc(mcpr,'dct.bit',50);
 disp("image_dct_dec")
 img_dec = image_dct_dec('dct.bit','frame_dec.jpg');
 % whos img_dec
@@ -33,7 +30,8 @@ imwrite(reconst, 'dustpan_reconst.jpg');
 img = imread('dustpan_reconst.jpg');
 writeVideo(outputVideo,img);
 
-i=i+1;
+frame1 = frame2;
+
 j=j+1
 end
 close(outputVideo);
